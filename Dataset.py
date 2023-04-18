e8ebf235204d9db75d266f976ee31c32e319be82
defense <1900013009@pku.edu.cn>
2023-04-18 17:13:40 +0800

    init

---
+++ Dataset.py
@@ -0,0 +1,42 @@
+import os
+import torch
+import torch.utils.data as data
+from PIL import Image
+import numpy as np
+
+class CUSTOM(data.Dataset):
+    def __init__(self, data_root,transform=None):
+        # specify annotation file for dataset
+        
+
+        self.transform = transform
+        self.data_root = data_root
+        self.input_size = (224,224)
+        self.output_size = (56,56) # heatmap size,should be 1/4 of image_size
+        self.scale_factor = 0.25
+
+        self.index_list=[f for f in sorted(os.listdir(data_root))]
+
+        self.mean = np.array([0.485, 0.456, 0.406], dtype=np.float32)
+        self.std = np.array([0.229, 0.224, 0.225], dtype=np.float32)
+
+    def __len__(self):
+        return len(self.index_list)
+
+    def __getitem__(self, idx):
+
+        image_path = os.path.join(self.data_root,
+                                  f"{self.index_list[idx]}")
+
+        img = np.array(Image.open(image_path).convert('RGB'), dtype=np.float32)
+        img = img.astype(np.float32)
+        img = (img/255.0 - self.mean) / self.std
+        img = img.transpose([2, 0, 1])
+        
+        return img, torch.zeros((1,1)), self.index_list[idx]
+
+
+
+
+if __name__ == '__main__':
+    pass
---
+++ predict.py
@@ -0,0 +1,71 @@
+# predict for custom dataset in facial landmark
+# ------------------------------------------------------------------------------
+# Copyright (c) Microsoft
+# Licensed under the MIT License.
+# Created by Tianheng Cheng(tianhengcheng@gmail.com)
+# ------------------------------------------------------------------------------
+
+import os
+import pprint
+import argparse
+
+from Dataset import CUSTOM
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+import models as models
+from config import config, update_config
+from utils import decode_preds,visualize_and_save_landmarks
+def parse_args():
+
+    parser = argparse.ArgumentParser(description='Train Face Alignment')
+
+    parser.add_argument('--cfg', help='experiment configuration filename',
+                        default="./setting/config.yaml", type=str)
+    parser.add_argument('--model-file', help='model parameters',
+                         default="./setting/HR18-WFLW.pth", type=str)
+    parser.add_argument('--data_root', help='path to data',default="./data", type=str)
+    parser.add_argument('--save_dir', help='path to data',default="./save_dir", type=str)
+
+    args = parser.parse_args()
+    update_config(config, args)
+    return args
+
+
+
+args = parse_args()
+
+config.freeze()
+model = models.get_face_alignment_net(config)
+gpus = list((0,)) # just using one gpu for interfence
+model = nn.DataParallel(model, device_ids=gpus).cuda()
+# load model
+state_dict = torch.load(args.model_file)
+if 'state_dict' in state_dict.keys():
+    state_dict = state_dict['state_dict']
+    model.load_state_dict(state_dict)
+else:
+    model.module.load_state_dict(state_dict)
+
+if not os.path.exists(args.save_dir):
+    os.mkdir(args.save_dir)
+
+test_loader = DataLoader(
+    dataset=CUSTOM(args.data_root),
+    batch_size=1,
+    shuffle=False,
+    num_workers=1
+)
+with torch.no_grad():
+    for i, (inp, _,image_name) in enumerate(test_loader):
+        output = model(inp)
+        score_map = output.data.cpu()
+        preds = decode_preds(score_map , [56, 56])
+        visualize_and_save_landmarks(
+            image_path=os.path.join(args.data_root,image_name),
+            preds=preds*4,
+            save_path=os.path.join("./experiments/res",image_name))
+
+
+
+
